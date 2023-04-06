142fafc12b702a3ac14a9739662a42a83b4017b8
cesardeltoroc <cesardeltorojr@gmail.com>
2023-04-05 22:11:15 -0700

    Working on implementing images into website and images within the boxes using MUI (Material UI)

---
+++ coffee-dogs/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <a
+          className="App-link"
+          href="https://www.dogshaming.com/"
+        >
+          Click Me
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ coffee-dogs/src/card.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Box from '@mui/material/Box';
+
+
+
+export default function LabelBottomNavigation() {
+    const [value, setValue] = React.useState('recents');
+
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+    };
+
+    return (
+        <BottomNavigation value={value} onChange={handleChange}>
+=            <Box
+                sx={{
+                    height: 300,
+
+                }}
+            >
+                <BottomNavigationAction
+                    label="Recents"
+                    value="recents"
+                    icon={<RestoreIcon />}
+                />
+                <BottomNavigationAction
+                    label="Favorites"
+                    value="favorites"
+                    icon={<FavoriteIcon />}
+                />
+                <BottomNavigationAction
+                    label="Nearby"
+                    value="nearby"
+                    icon={<LocationOnIcon />}
+                />
+            </Box>
+        </BottomNavigation>
+    );
+}
